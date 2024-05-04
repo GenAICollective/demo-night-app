69d3344477ef0ca450692af4bdfed7cd8e8c6e8e
Chappy Asel <chappy_asel@hotmail.com>
2024-05-03 17:55:35 -0700

    Basic presentation infra

---
+++ src/app/admin/presention/components/Presentations.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import QRCode from "react-qr-code";
+
+import { type CurrentEvent } from "~/server/api/routers/event";
+
+import useEventSync from "~/app/(attendee)/hooks/useEventSync";
+
+export default function Presentations({
+  currentEvent: initialCurrentEvent,
+}: {
+  currentEvent: CurrentEvent;
+}) {
+  const { currentEvent, event } = useEventSync(initialCurrentEvent);
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 font-kallisto font-bold">
+      <h1 className="text-4xl">{currentEvent.name}</h1>
+      <QRCode size={256} value={window.location.origin} />
+    </div>
+  );
+}
--- src/app/admin/presention/page.tsx
+++ src/app/admin/presention/page.tsx
@@ -1,3 +1,30 @@
-export default function AdminPresentPage() {
-  return <div>Present</div>;
+import Image from "next/image";
+
+import { api } from "~/trpc/server";
+
+import Presentations from "./components/Presentations";
+
+export async function generateMetadata() {
+  const currentEvent = await api.event.getCurrent();
+  return { title: currentEvent?.name ?? "GenAI Collective Demo Night!" };
+}
+
+export default async function AdminPresentPage() {
+  const currentEvent = await api.event.getCurrent();
+  if (!currentEvent) {
+    return (
+      <main className="flex min-h-screen w-full flex-col items-center justify-center pb-16 font-kallisto text-black">
+        <Image src="/images/logo.png" alt="logo" width={160} height={160} />
+        <h1 className="pt-4 text-center text-2xl font-semibold">
+          No active demo night 🥲
+        </h1>
+        <p className="text-lg font-medium italic">(hold tight!)</p>
+      </main>
+    );
+  }
+  return (
+    <main className="flex size-full min-h-screen flex-col items-center justify-center">
+      <Presentations currentEvent={currentEvent} />
+    </main>
+  );
 }
