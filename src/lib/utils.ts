0dbfb38c45774ec3408f37d9c181cc450f31813d
Chappy Asel <chappy_asel@hotmail.com>
2025-02-01 05:58:30 -0800

    Fix utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,24 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatDate(inputDate: string | number | Date) {
+  const date = new Date(inputDate);
+  return date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+}
+
+export function debounce<T extends Function>(cb: T, wait = 1000) {
+  let h = 0;
+  const callable = (...args: any) => {
+    clearTimeout(h);
+    h = window.setTimeout(() => cb(...args), wait);
+  };
+  return callable as unknown as T;
 }
